5da82584a1d1532a6c27ac2c9bdd6244528f6a88
saif sayed <saifsayed@outlook.com>
2018-10-31 14:58:50 -0500

    updated main.py and client/client.py. Functionality includes main sending data to client and client acknowledging it and asking for next data. Info is exchanged using JSON. Main has some sample messages for testing. Currently if anything fails on the client side the acknowledment of failure is given to the main but nothing happens and the next message is sent to the client when the client is ready to recieve it.

--- client/client.py
+++ client/client.py
@@ -1,18 +1,16 @@
 from docx import Document
+import json
+import os
 
 
 # TODO: Add more functionality
 class DocumentWriter:
 
-    def __init__(self, docname=None):
+    def __init__(self, docname):
         self._current_para = None
-        if docname:
-            self._document = Document(docname)
-            self._docname = docname
-        else:
-            self._document = Document()
-            self._docname = "generated.docx"
-        self.add_paragraph(text="")
+        self.error_code = 0
+        self._document = Document()
+        self._docname = docname
 
     def add_text(self, text):
         self._current_para.add_run(text=text)
@@ -26,26 +24,73 @@ class DocumentWriter:
     def save_document(self):
         self._document.save(self._docname)
 
-
-name = "demo.docx"
-
-doc = DocumentWriter()
-doc.add_heading("Hello World!")
-doc.add_paragraph("New Paragraph \n")
-doc.add_text("This is so Awesome1!")
-doc.add_text("This is so Awesome2!")
-doc.add_text("This is so Awesome3!")
-doc.add_text("This is so Awesome4!")
-doc.save_document()
-
+def parse_message(message_from_gce):
+    json_data = json.loads(message_from_gce)
+    message_clientID = str(json_data['clientID'])
+    message_filename = str(json_data['filename'])
+    message_command = str(json_data['command'])
+    message_value = str(json_data['text'])
+    message_count = str(json_data['counter'])
+    return message_clientID, message_filename, message_command, message_value, message_count
 
 # TODO: Configure Websocket connection to Cloud server
-# from websocket import create_connection
-# ws = create_connection("ws://35.237.62.22:8080/websocket")
-# print("Sending 'Hello, World'...")
-# ws.send("Hello, World")
-# print("Sent")
-# print("Receiving...")
-# result = ws.recv()
-# print("Received '%s'" % result)
-# ws.close()
+from websocket import create_connection
+ws = create_connection("ws://localhost:8080/websocket")
+iter = 0
+
+client_busy = False
+client_online = True
+message_from_cloud = []
+
+while True:
+    if not client_online:
+        ws.send("client offline")
+        print("client offline")
+
+    if client_busy:
+        ws.send("client busy")
+        message_clientID, message_filename, message_command, message_value, message_count = parse_message(message_from_cloud)
+        document_name = 'outputs/' + message_filename + '.docx'
+        doc = DocumentWriter(docname=document_name)
+
+        #Create a file
+        if message_command == 'create':
+            try:
+                doc.add_paragraph(message_value)
+                doc.save_document()
+                ws.send("Message %s processed, 0" % message_count)
+                client_busy = False
+            except:
+                ws.send("Message %s processed, 1" % message_count)
+                client_busy = False
+
+        # Edit an existing File
+        if message_command == 'edit':
+            try:
+                doc.add_paragraph(message_value)
+                doc.save_document()
+                ws.send("Message %s processed, 0" % message_count)
+                client_busy = False
+            except:
+                ws.send("Message %s processed, 1" % message_count)
+                client_busy = False
+
+        # Delete a file
+        if message_command == 'delete':
+            try:
+                os.remove(document_name)
+                ws.send("Message %s processed, 0" % message_count)
+                client_busy = False
+            except OSError:
+                ws.send("Message %s processed, 1" % message_count)
+                client_busy = False
+
+    if not client_busy and client_online:
+        print("Sending 'ready to receive from client'")
+        ws.send("client_online and not busy")
+        print("Sent to cloud")
+        print("Waiting for data from cloud")
+        message_from_cloud = ws.recv()
+        print("Message Received from cloud")
+        print(message_from_cloud)
+        client_busy = True
--- main.py
+++ main.py
@@ -28,39 +28,86 @@ class InputRecords:
 
     def __init__(self):
         self.dialogue = {}
+        self.message_to_client = []
+        self.message_iter = 0
 
-    def write(self):
-        input_data = request.body.read().decode("utf-8")
-        json_data = json.loads(input_data)
-        inputs = json_data['inputs']
-        raw_inputs = inputs[0]['rawInputs']
-        raw_query = raw_inputs[0]['query'].split(" ")
-        dialogue_key = raw_query[0]
-        dialogue_value = raw_query[1:len(raw_query)]
-        dialogue_value = " ".join(dialogue_value)
-        self.dialogue['command'] = dialogue_key
+    def create_json(self, message):
+        dialoge_clientID, dialogue_command, dialogue_filename, dialogue_value, dialogue_count = message.split(',')
+        self.dialogue['clientID'] = dialoge_clientID
+        self.dialogue['filename'] = dialogue_filename
+        self.dialogue['command'] = dialogue_command
         self.dialogue['text'] = dialogue_value
+        self.dialogue['counter'] = dialogue_count
         dialogue_json = json.dumps(self.dialogue)
-        loaded_dialogue = json.loads(dialogue_json)
-        # print(loaded_dialogue)
-        return loaded_dialogue
+        return dialogue_json
 
     def handle_websocket(self):
         wsock = request.environ.get('wsgi.websocket')
         if not wsock:
             abort(400, 'Expected WebSocket request.')
 
+        ready_to_receive = False
+        waiting_for_client_ack = False
+
         while True:
             try:
-                message = wsock.receive()
-                wsock.send("Your message was: %r" % message)
+                message_from_client = str(wsock.receive())
+
+                if message_from_client == 'client offline':
+                    print('client offline')
+                    ready_to_receive = False
+
+                if message_from_client == 'client_online and not busy':
+                    ready_to_receive = True
+
+                if message_from_client == 'client busy':
+                    waiting_for_client_ack = True
+
+                if message_from_client.split(',')[0] == ('Message %d processed' % self.message_iter):
+                    error_code = int(message_from_client.split(',')[1])
+                    if error_code == 0:
+                        print('Word File succesfully updated for message %s' % self.message_to_client[self.message_iter])
+                        waiting_for_client_ack = False
+                        ready_to_receive = True
+                        self.message_iter += 1
+                    else:
+                        print('Word File update failure for message %s' % self.message_to_client[self.message_iter])
+                        # based on the user feedback this message on the iter needs needs to be updated and resent. Current logic ignores the error and moves forward with next message
+                        waiting_for_client_ack = False
+                        ready_to_receive = True
+                        self.message_iter += 1
+
+                if ready_to_receive and not waiting_for_client_ack:
+                    print('Sending from cloud')
+                    send_message_to_client = self.create_json(self.message_to_client[self.message_iter])
+                    wsock.send(send_message_to_client)
+                    print('Message sent %s' % send_message_to_client)
+                    waiting_for_client_ack = True
+
+                else:
+                    continue
+
             except WebSocketError:
+                print('Sending to client Error')
                 break
 
-
 myApp = InputRecords()
-bottle.route('/write', 'POST', myApp.write)
+# bottle.route('/write', 'POST', myApp.write)
+
+# wordx_cly means xth file on client y
+word_commands = ["1,create,word1_cl1,My First Line on client 1,0",
+                 "1,edit,word1_cl1, My Second Line on client 1,1",
+                 "2,create,word1_cl2,My Second File First Line,2",
+                 "1,delete,word1_cl1, ,3",
+                 "2,delete,word2_cl2, ,4",
+                 "1,edit,word1_cl1,My Third Line on client 1,5",
+                 "2,edit,word1_cl2,My Third Line on client 2,6",
+                 "2,create,word4_cl2,My First Line on client 2, 7"]
+
+for iter in range(len(word_commands)):
+    myApp.message_to_client.append(word_commands[iter])
+
 bottle.route('/websocket', 'GET', myApp.handle_websocket)
 
-server = WSGIServer(("0.0.0.0", 8080), bottle, handler_class=WebSocketHandler)
+server = WSGIServer(("localhost", 8080), bottle, handler_class=WebSocketHandler)
 server.serve_forever()
